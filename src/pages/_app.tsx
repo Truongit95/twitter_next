3bc559785aedfe0f503a90724e3b68b613993505
LuLuTV <nguyennhuttruong95@gmail.com>
2023-04-03 21:05:20 +0700

    main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../../components/Layout";
+import LoginModel from "../../components/models/LoginModel";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+   <>
+   <LoginModel/>
     <Layout>
        <Component {...pageProps} />
     </Layout>
+   </>
   );
 }
