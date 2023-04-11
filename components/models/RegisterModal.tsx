28e29cd882de0885a661d6a67565143c5f39455f
LuLuTV <nguyennhuttruong95@gmail.com>
2023-04-11 22:06:55 +0700

    user

--- components/models/RegisterModal.tsx
+++ components/models/RegisterModal.tsx
@@ -34,7 +34,7 @@ const RegisterModal = () => {
         await axios.post("/api/register", {
           email,
           password,
-          username:userName,
+          username: userName,
           name
         });
 
@@ -43,7 +43,7 @@ const RegisterModal = () => {
         signIn("credentials", {
           email,
           password,
-          userName,
+          username: userName,
           name
         });
 
