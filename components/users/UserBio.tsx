c8d9ec2c8b6a12d13de1f1b7914340eff29ed0f2
LuLuTV <nguyennhuttruong95@gmail.com>
2023-04-16 12:55:55 +0700

    o

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
+import { format } from 'date-fns'
 import useUser from "../../hook/useUser";
 import useCurrentUser from "../../hook/useCurrentUser";
+import Button from "../Button";
 
 interface UserBioProps {
     userId: string
@@ -9,6 +11,22 @@ interface UserBioProps {
 const UserBio: FC<UserBioProps> = ({ userId }) => {
     const { data: fetcherUser } = useUser(userId);
     const { data: currentUser } = useCurrentUser();
-    return (<></>);
+
+    const createdAt = useMemo(() => {
+        if (!currentUser?.createdAt)
+            return null;
+        return format(new Date(currentUser.createdAt), 'MMMM yyyy');
+    }, [currentUser?.createdAt]);
+    return (
+        <div className="border-b-[1px] border-neutral-800 pb-4">
+            <div className="flex justify-end p-2">
+                {currentUser?.id === userId ? (
+                    <Button secondary label="Edit" onClick={() => { }} />
+                ) : (
+                    <Button secondary label="Follow" onClick={() => { }} />
+                )}
+            </div>
+        </div>
+    );
 }
 export default UserBio
